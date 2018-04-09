805a8e711f7e55733f41af46d1e1be92a7a10f90
Ryan Pierce <ryanwp9190@gmail.com>
2018-04-08 20:45:59 -0400

    Added decompression

--- LoadImdbToMongoDB/Program.cs
+++ LoadImdbToMongoDB/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,37 @@ namespace LoadImdbToMongoDB
     {
         static void Main(string[] args)
         {
+
+            FileInfo[] files;
+
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\Ryan-P\Source\Repos\mongomovie2\imdb");
+            files = directory.GetFiles();
+
+            foreach (var file in files)
+            {
+                FileInfo fileToDecompress = file;
+
+                using (FileStream originalFileStream = fileToDecompress.OpenRead())
+                {
+                    string currentFileName = fileToDecompress.FullName;
+                    string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                    using (FileStream decompressedFileStream = File.Create(newFileName))
+                    {
+
+                        using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                        {
+                            decompressionStream.CopyTo(decompressedFileStream);
+                            Console.WriteLine("Decompressed: {0}", fileToDecompress);
+                        }
+
+                    }
+                }
+                
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
