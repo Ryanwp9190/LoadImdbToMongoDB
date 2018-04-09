65d1e7c9fc8f666606466be58686f5366824fa4c
Ryan Pierce <ryanwp9190@gmail.com>
2018-04-08 20:53:07 -0400

    added fileinfo

--- LoadImdbToMongoDB/Program.cs
+++ LoadImdbToMongoDB/Program.cs
@@ -22,18 +22,23 @@ namespace LoadImdbToMongoDB
             {
                 FileInfo fileToDecompress = file;
 
+                long size = fileToDecompress.Length;
+
                 using (FileStream originalFileStream = fileToDecompress.OpenRead())
                 {
                     string currentFileName = fileToDecompress.FullName;
                     string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
 
+                    Console.WriteLine("File "+ currentFileName + " is " + size + " before decompression.");
+
                     using (FileStream decompressedFileStream = File.Create(newFileName))
                     {
-
                         using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
                         {
                             decompressionStream.CopyTo(decompressedFileStream);
+                            long newSize = newFileName.Length;
                             Console.WriteLine("Decompressed: {0}", fileToDecompress);
+                            Console.WriteLine("Decompressed file size is " + newSize);
                         }
 
                     }
